0cef263b9b6496216b731949606cfc9c1f830f47
Olivier Loschi <o.loschi@gmail.com>
2015-02-04 16:42:44 +0100

    added plot2

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ source("load_data.R")
 png("plot1.png", width = 480, height = 480)
 
 #Make Plot
-hist(hpc_12Feb$Global_active_power, main = "Global Active power", col = "red", xlab = "Global Active Power (kilowatts)", )
+hist(hpc_12Feb$Global_active_power, main = "Global Active power", col = "red", xlab = "Global Active Power (kilowatts)")
 
 #close png graphic device
 dev.off()
---
+++ plot2.R
@@ -0,0 +1,14 @@
+#Load data
+source("load_data.R")
+
+#As my windows system is not english, i need to force the locale so the name of the days will be in english on the plot
+Sys.setlocale("LC_ALL", "english")
+
+#open png graphic device with size 480*480
+png("plot2.png", width = 480, height = 480)
+
+#Make Plot
+plot(hpc_12Feb$Time, hpc_12Feb$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+
+#close png graphic device
+dev.off()
